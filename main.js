b823451d146ec9ef589d1d825938a96ebcbd1e8a
Margarita <gorbashevarira@gmail.com>
2026-03-19 21:28:53 +0300

    Step 12: conditional statements in JavaScript

--- main.js
+++ main.js
@@ -1,158 +1,226 @@
-// console.log("Hello, JavaScript!")
-// let age = 20;
-// let name = "Denis";
-// let isStudent = true;
+console.log("Hello, JavaScript!")
+let age = 20;
+let name = "Denis";
+let isStudent = true;
 
-// console.log("Name: ", name);
-// console.log("Age: ", age);
-// console.log("Is student: ", isStudent)
+console.log("Name: ", name);
+console.log("Age: ", age);
+console.log("Is student: ", isStudent)
 
-// let value = 10;
-// console.log(value);
-// value = "Теперь это строка";
-// console.log(value);
-// value = true;
-// console.log(value);
+let value = 10;
+console.log(value);
+value = "Теперь это строка";
+console.log(value);
+value = true;
+console.log(value);
 
-// let userName = "Алексей";
-// console.log('Привет, ${userName}!');
+let userName = "Алексей";
+console.log('Привет, ${userName}!');
 
-// let price = 99.99;
-// let temperature = -15;
-// let infinity = 1 / 0;
-// let notANumber = 0 / 0;
-// console.log(0.1 + 0.2);
-// let bigNumber = 9007199254740991n;
-// let huge = BigInt("7895456212459878955613216487898554554487");
-// let isAlive = true;
-// let isWorking = false;
-// let isAdult = age >= 18;
-// let x;
-// let y = undefined;
-// let userData = null;
-// let id = Symbol("id");
+let price = 99.99;
+let temperature = -15;
+let infinity = 1 / 0;
+let notANumber = 0 / 0;
+console.log(0.1 + 0.2);
+let bigNumber = 9007199254740991n;
+let huge = BigInt("7895456212459878955613216487898554554487");
+let isAlive = true;
+let isWorking = false;
+let isAdult = age >= 18;
+let x;
+let y = undefined;
+let userData = null;
+let id = Symbol("id");
 
-// let person = {
-//     name: "Станислав",
-//     age: 30,
-//     isStudent: false,
-//     sayHello: function () {
-//         console.log("Привет!");
-//     },
-// };
-// console.log(person.name);
+let person = {
+    name: "Станислав",
+    age: 30,
+    isStudent: false,
+    sayHello: function () {
+        console.log("Привет!");
+    },
+};
+console.log(person.name);
 
-// let fruits = ["яблоко", "банан", "фпельсин"];
-// let number = [1,2,3,4,5];
-// let mixed = ["текст", 42, true, null];
+let fruits = ["яблоко", "банан", "фпельсин"];
+let number = [1,2,3,4,5];
+let mixed = ["текст", 42, true, null];
 
-// function sum(a, b) {
-//     return a + b;
-// }
-// let multiply = function(x, y) {
-//     return x * y;
-// }
+function sum(a, b) {
+    return a + b;
+}
+let multiply = function(x, y) {
+    return x * y;
+}
 
-// console.log(sum(5,3));
+console.log(sum(5,3));
 
-// let now = new Date();
-// let birthday = new Date("1995-12-17");
+let now = new Date();
+let birthday = new Date("1995-12-17");
 
-// let a = 10;
-// let b = 3;
-// console.log(a + b);
-// console.log(a - b);
-// console.log(a * b);
-// console.log(a / b);
+let a = 10;
+let b = 3;
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
 
-// console.log(10 + "5");
-// console.log("10" - 5);
+console.log(10 + "5");
+console.log("10" - 5);
 
-// const numberArray = [1, 2, 3];
-// numberArray[0] = 10;
-// console.log(numberArray);
-// //numberArray = [5, 6, 7];
+const numberArray = [1, 2, 3];
+numberArray[0] = 10;
+console.log(numberArray);
+//numberArray = [5, 6, 7];
 
-// const persons = {name: "Denis", age: 18};
-// persons.age = 50;
-// persons.city = "Volgograd";
-// console.log(persons);
-// person = {name: "Stas"};
+const persons = {name: "Denis", age: 18};
+persons.age = 50;
+persons.city = "Volgograd";
+console.log(persons);
+person = {name: "Stas"};
 
-// console.log(typeof "текст");
-// console.log(typeof 42);
-// console.log(typeof true);
-// console.log(typeof undefined);
-// console.log(typeof null);
-// console.log(typeof {});
-// console.log(typeof []);
-// console.log(typeof function () {});
+console.log(typeof "текст");
+console.log(typeof 42);
+console.log(typeof true);
+console.log(typeof undefined);
+console.log(typeof null);
+console.log(typeof {});
+console.log(typeof []);
+console.log(typeof function () {});
 
-// let numberX = null;
-// console.log(numberX === null);
+let numberX = null;
+console.log(numberX === null);
 
-// let newPrice = 1;
-// console.log(newPrice);
-// console.log(typeof newPrice);
-// newPrice = "один";
-// console.log(newPrice);
-// console.log(typeof newPrice);
+let newPrice = 1;
+console.log(newPrice);
+console.log(typeof newPrice);
+newPrice = "один";
+console.log(newPrice);
+console.log(typeof newPrice);
 
-// let num = 42;
-// let str = String(num);
-// let str2 = num.toString();
-// let str3 = +"99";
-// let strNum = "123";
-// let int = Number(strNum);
-// let int2 = parseInt("42.5");
-// let float = parseFloat("3.14");
-// let int3 = +"99";
+let num = 42;
+let str = String(num);
+let str2 = num.toString();
+let str3 = +"99";
+let strNum = "123";
+let int = Number(strNum);
+let int2 = parseInt("42.5");
+let float = parseFloat("3.14");
+let int3 = +"99";
 
-// let bool1 = Boolean(1);
-// let bool2 = !!1;
-// let bool3 = Boolean(0);
-// let bool4 = Boolean("");
+let bool1 = Boolean(1);
+let bool2 = !!1;
+let bool3 = Boolean(0);
+let bool4 = Boolean("");
 
-// console.log("5" + 3);
-// console.log("5" - 3);
-// console.log("5" * "2");
-// console.log(true + 1);
-// console.log(false + 1);
-// console.log(null + 1);
-// console.log(undefined + 1);
+console.log("5" + 3);
+console.log("5" - 3);
+console.log("5" * "2");
+console.log(true + 1);
+console.log(false + 1);
+console.log(null + 1);
+console.log(undefined + 1);
 
-// console.log(5 == "5");
-// console.log(5 === "5");
-// console.log(0 == false);
-// console.log(0 === false);
-// console.log(null == undefined);
-// console.log(null === undefined);
+console.log(5 == "5");
+console.log(5 === "5");
+console.log(0 == false);
+console.log(0 === false);
+console.log(null == undefined);
+console.log(null === undefined);
 
-// let obj1 = {name: "John"};
-// let obj2 = {name: "John"};
+let obj1 = {name: "John"};
+let obj2 = {name: "John"};
 
-// console.log(obj1 == obj2);
-// console.log(obj1 === obj2);
+console.log(obj1 == obj2);
+console.log(obj1 === obj2);
 
-// let obj3 = obj1;
-// console.log(obj1 === obj2);
+let obj3 = obj1;
+console.log(obj1 === obj2);
 
-// let arr1 = [1, 2, 3];
-// let arr2 = [1, 2, 3];
-// console.log(arr1 === arr2);
+let arr1 = [1, 2, 3];
+let arr2 = [1, 2, 3];
+console.log(arr1 === arr2);
 
-// console.log("Обычное сообщение");
-// console.warn("Предупреждение!");
-// console.error("Ошибка!");
-// console.info("Иноформация!");
-// let users = [
-//     {name: "John", age: 30},
-//     {name: "John", age: 25},
-// ];
-// console.table(users);
-// console.clear();
+console.log("Обычное сообщение");
+console.warn("Предупреждение!");
+console.error("Ошибка!");
+console.info("Иноформация!");
+let users = [
+    {name: "John", age: 30},
+    {name: "John", age: 25},
+];
+console.table(users);
+console.clear();
 
 let a2 = 3;
 let b2 = 4;
 console.log("Сумма: ",3 + 4);
 console.log("Произведение: ", 3 * 4);
+
+let yourAge = 18;
+if (yourAge >= 18) {
+    console.log("Доступ разрешён");
+} else {
+    console.log("Доступ запрещён");
+}
+
+let temperature1 = 10;
+if (temperature1 < 0) {
+    console.log("Холодно")
+} else if (0 < temperature1 < 20) {
+    console.log("Прохладно");
+} else {
+    console.log("Тепло");
+}
+
+
+if (isStudent && age < 25) {
+    console.log("Доступна студенческая скидка");
+}
+
+
+let isLoggedIn = false;
+let isAdmin = true;
+
+if (isLoggedIn && isAdmin) {
+    console.log("Полный доступ");
+} else if (isLoggedIn && isAdmin == false) {
+    console.log("Ограниченный доступ");
+} else if (isLoggedIn == false) {
+    console.log("Доступ запрещен");
+}
+
+
+let a3 = 10;
+let b3 = "10";
+console.log(a3 == b3);
+console.log(a3 === b3);
+
+let message = age >= 18 ? "Совершеннолетний" : "Несовершеннолетний";
+console.log(message);
+
+let day = 3;
+
+switch (day) {
+  case 1: console.log("Понедельник"); break;
+  case 2: console.log("Вторник"); break;
+  case 3: console.log("Среда"); break;
+  default: console.log("Неизвестный день");
+}
+
+let monthNumber = 7;
+switch (monthNumber) {
+  case 1: console.log("Январь"); break;
+  case 2: console.log("Февраль"); break;
+  case 3: console.log("Март"); break;
+  case 4: console.log("Апрель"); break;
+  case 5: console.log("Май"); break;
+  case 6: console.log("Июнь"); break;
+  case 7: console.log("Июль"); break;
+  case 8: console.log("Август"); break;
+  case 9: console.log("Сентябрь"); break;
+  case 10: console.log("Октябрь"); break;
+  case 11: console.log("Ноябрь"); break;
+  case 12: console.log("Декабрь"); break;
+  default: console.log("Некорректное значение месяца.");
+}
